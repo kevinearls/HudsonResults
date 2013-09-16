2cce46d55414f5423ce92c49c779d6343716f7c7
Kevin Earls <kevin@kevinearls.com>
2013-09-16 12:13:00 +0200

    Finish rename

--- go.sh
+++ go.sh
@@ -1,5 +1,4 @@
 mvn clean jaxb2:xjc  install
-mvn exec:java -Dexec.mainClass=com.kevinearls.hudson.SummarizeBuildResults -Dexec.args="/Users/kearls/mytools/HudsonResultsAnalyzer/jobs .*6[-\.]1.*platform"
-mvn exec:java -Dexec.mainClass=com.kevinearls.hudson.SummarizeBuildResults -Dexec.args="/Users/kearls/mytools/HudsonResultsAnalyzer/jobs .*7[-\.]2.*checkin"
+mvn exec:java -Dexec.mainClass=com.kevinearls.hudson.SummarizeBuildResults -Dexec.args="/Users/kearls/mytools/data/jobs .*6[-\.]1.*platform"
 #open result.html
 #ls -tr result*.html | tail -1 | xargs open
